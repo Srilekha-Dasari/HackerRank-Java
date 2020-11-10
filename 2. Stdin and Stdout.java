5acfc3cfdcf932da0f131073e10cfce6eacb10c5
Srilekha-Dasari <59114223+Srilekha-Dasari@users.noreply.github.com>
2020-11-10 15:06:12 +0000

    Create 2. Stdin and Stdout.java

---
+++ 2. Stdin and Stdout.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt(); //read int values
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+        System.out.println(a); //print the values
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
